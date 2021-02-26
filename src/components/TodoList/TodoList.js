59e79ba4ee1de94600893cf88214e6de132dc99c
TrungyuD <shawnkool10@gmail.com>
2021-02-27 04:09:01 +0700

    bulk action

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -7,6 +7,7 @@ const TodoList = ({tasks}) => {
     const sortTasks = tasks.sort((a,b) => {
         return new Date(a.date).getTime() - new Date(b.date).getTime() 
     })
+    
     return (
         <div className="list">
             <div className="content-list">
@@ -20,6 +21,23 @@ const TodoList = ({tasks}) => {
                 }
                 </div>     
             </div>
+
+            {
+                true && 
+                <div className="bottom-list">
+                    <p className="title-bulk">Bulk Action:</p>
+                    <div className="right-bottom">
+                        <button className="btn-done-bottom">
+                            <span className="text-btn-bottom">Done</span>
+                            </button>
+                            <button className="btn-remove-bottom">
+                            <span  className="text-btn-bottom">Remove</span>
+                        </button>
+                    </div>
+                </div>
+            }
+
+
         </div>
     )
 }
