5b1372cd374256b0c75ab2c927838728f82a0406
TrungyuD <shawnkool10@gmail.com>
2021-02-27 00:48:57 +0700

    logic add task

--- src/components/NewTask/DatePiority/DatePiority.js
+++ src/components/NewTask/DatePiority/DatePiority.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import calendar from '../../image/calendar.png';
 import arrow from '../../image/arrow.png';
 import './DatePiority.css';
 
-const DatePiority = () => {
+const DatePiority = (props) => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [piority, setPiority] = useState('Normal');
+    const handleChangeDate = (date) => {
+        setStartDate(date);
+        props.date(date);
+    }
+    const changePiority = (value) => {
+        setPiority(value);
+        props.piority(value);
+    }
     return (
         <div className="date-piority">
             <div className="date">
                 <label className="title-label">Due Date</label>
                 <div className="date-icon">
-                    <DatePicker />
+                    <DatePicker dateFormat="dd MMM yyyy"
+                        selected={startDate} onChange={handleChangeDate} />
                     <span className="box-icon">
                         <img className="icon-calendar" alt="" src={calendar} />
                     </span>
@@ -22,7 +33,7 @@ const DatePiority = () => {
                 <div className="dropdown-piority">
                     <div className="piority-icon">
                         <div className="choose">
-                            <p className="text-choose"> Normal
+                            <p className="text-choose"> {piority}
                                 <span className="icon-piority">
                                     <img className="icon-dropdown" src={arrow} alt="" />
                                 </span>
@@ -30,9 +41,9 @@ const DatePiority = () => {
                         </div>
                         <div className="list-piority">
                             <ul className="content-piority">
-                                <li className="item-piority" id="low">Low</li>
-                                <li className="item-piority" id="normal">Normal</li>
-                                <li className="item-piority" id="high">High</li>
+                                <li className="item-piority" id="low" onClick={() => changePiority("Low")}>Low</li>
+                                <li className="item-piority" id="normal" onClick={() => changePiority("Normal")}>Normal</li>
+                                <li className="item-piority" id="high" onClick={() => changePiority("High")}>High</li>
                             </ul>
                         </div>
                     </div>
--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewTask.css';
 import DatePiority from './DatePiority/DatePiority';
 
 const NewTask = () => {
+    const [task, setTask] = useState({
+        title: "",
+        description: "",
+        date: new Date(),
+        piority: ""
+    });
+    const handleChange = (e) => {
+        setTask({...task, [e.target.name]:e.target.value})
+    }
+    const addNewTask = () => {
+        console.log(task);
+    }
+    const setDate = (date) => {
+        setTask({...task, date});
+    }
+    const setPiority = (piority) => {
+        setTask({...task, piority});
+    }
     return (
         <div className="new-task">
             <p className="title">New Task</p>
             <div className="add-new-task">
-                <input type="text" placeholder="Add new task ..."/>
+                <input type="text" name="title" onChange={handleChange} placeholder="Add new task ..."/>
             </div>
             <div className="description">
-                <label for="description-input" className="title-label">Description</label>
-                <textarea type="text"/>
+                <label className="title-label">Description</label>
+                <textarea type="text" name="description" onChange={handleChange}/>
             </div>
-            <DatePiority />
-            <button className="btn-add">
+            <DatePiority date={setDate} piority={setPiority}/>
+            <button className="btn-add" onClick={addNewTask}>
                 <span className="text-btn">Add</span>
             </button>
         </div>
